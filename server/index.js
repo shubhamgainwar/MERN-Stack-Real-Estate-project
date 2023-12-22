feea356b679b7b59598870a4b100c56e91da083d
shubhamgainwar <gainwarshubham@gmail.com>
2023-12-23 01:43:14 +0530

    Created a middleware commit nine

--- server/index.js
+++ server/index.js
@@ -23,3 +23,13 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
