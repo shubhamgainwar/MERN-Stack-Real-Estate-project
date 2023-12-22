079980206db69d7fdaa7610498bd7e1d61e167f2
shubhamgainwar <gainwarshubham@gmail.com>
2023-12-22 21:10:37 +0530

    connect to database commit five

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
