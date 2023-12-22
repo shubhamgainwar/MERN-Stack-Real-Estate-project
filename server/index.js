810402387b7f6c624c210ed0184385f0c9f0ec0c
shubhamgainwar <gainwarshubham@gmail.com>
2023-12-23 01:04:21 +0530

    sign up api route commit eight

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -15,7 +16,10 @@ mongoose
   });
 
 const app = express();
-const port = 3000;
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
