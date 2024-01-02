adb90aaaa0f8ce59c9f82c37d776531940b5731c
shubhamgainwar <gainwarshubham@gmail.com>
2024-01-02 19:44:58 +0530

    create update user api route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 import path from "path";
 
 dotenv.config();
@@ -18,6 +19,7 @@ mongoose
 const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 const port = process.env.PORT || 3000;
 
